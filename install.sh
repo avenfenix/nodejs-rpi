38a0caaf0adcb68ef3a1aba4d0369eb6c6b2856e
Juan Parra <thedonjuan008p@gmail.com>
2023-10-15 11:17:01 -0300

    0.0.0.1v

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y ca-certificates curl gnupg
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
-NODE_MAJOR=20
+NODE_MAJOR=18
 echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install nodejs
